34f7643accdb282f387ae6e11cb4d24e955c529a
J.D. Bean <jonathandbean@gmail.com>
2018-02-22 18:46:47 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,32 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+
+
+
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def list(songs)
+ songs.each_with_index do |title, index|
+   puts "#{index + 1}. #{title}"
+ end
+end
+
+def play(songs)
+
+
+end
+
+def exit_jukebox
+
+end
+
+def run(songs)
+  help
+end
